57fa0b8225ed020c17b5d25b4a58911f6f287827
Ash_258 <Ash_258@example.com>
2023-08-01 20:48:27 +0800

    use `exec fish` in `~/.bashrc`

--- home/peter/.config/fish/conf.d/2023_07.fish
+++
@@ -1,9 +0,0 @@
-# Add at 2023/07/23, for using emacs at default editor 
-function fish_about_emacs
-    set -U EDITOR emacs
-end
-
-# Add at 2023/07/23, for exercism
-function fish_about_exercism
-        set -x PATH ~/bin:$PATH $PATH
-end
--- home/peter/.config/fish/conf.d/before_230727.fish
+++
@@ -1,9 +0,0 @@
-set -x RUSTUP_UPDATE_ROOT https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup
-set -x RUSTUP_DIST_SERVER https://mirrors.tuna.tsinghua.edu.cn/rustup
-
-# Add on 2023/07/17 for guix add to shell profile
-#if status is-interactive
-#    set -x GUIX_PROFILE "$HOME/.guix-profile"
-#    . "$GUIX_PROFILE/etc/profile"
-#end
-# But fish not support ${ syntax, need `fenv` in `fish-foreign-env` package.
